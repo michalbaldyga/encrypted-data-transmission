422166413c96241da240ce07f57f36972ef440e3
michalbaldyga <michal.baldyga01@gmail.com>
2023-04-15 16:38:58 +0200

    Add threads

--- client.py
+++ client.py
@@ -1,11 +1,10 @@
 import socket
+import threading
+from message import sending_messages, receiving_messages
+from constants import *
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
+threading.Thread(target=sending_messages, args=(client, )).start()
+threading.Thread(target=receiving_messages, args=(client, )).start()
--- server.py
+++ server.py
@@ -1,16 +1,12 @@
 import socket
+import threading
+from message import sending_messages, receiving_messages
+from constants import *
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+client, _ = server.accept()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+threading.Thread(target=sending_messages, args=(client, )).start()
+threading.Thread(target=receiving_messages, args=(client, )).start()
