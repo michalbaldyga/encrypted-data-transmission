5ef461dc2f464de20b6a430f84bcea44c075a137
BaranskiKacper <kbaranski20@gmail.com>
2023-04-16 15:56:03 +0200

    Added public keys exchange

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 import threading
-from constants import HOST, PORT
+from constants import HOST, PORT, SEPARATOR, PUBLIC_KEY
 from message import send, recv
+from utils import init_keys
 
 # create the client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,6 +11,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(f"[+] Connecting to {HOST}:{PORT}")
 client.connect((HOST, PORT))
 print("[+] Connected.")
+# type password
+password = input("Password: ")
+public_key, private_key = init_keys(PORT, password)
+client.send(f"{PUBLIC_KEY}{SEPARATOR}{public_key}".encode())
 
 threading.Thread(target=send, args=(client,)).start()
 threading.Thread(target=recv(client, (HOST, PORT)), args=(client,)).start()
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import threading
 import socket
-from constants import HOST, PORT, BUFFER_SIZE, MESSAGE_TAG, SEPARATOR, FILE_TAG
+from constants import HOST, PORT, BUFFER_SIZE, MESSAGE_TAG, SEPARATOR, FILE_TAG, PUBLIC_KEY
 from message import send, recv
+from utils import init_keys
 
 # create the server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,10 +13,12 @@ server.bind((HOST, PORT))
 # enabling our server to accept connections
 server.listen()
 print(f"[*] Listening as {HOST}:{PORT}")
-
+# type password
+password = input("Password: ")
 # accept connection if there is any
 client, address = server.accept()
 print(f"[+] {address} is connected.")
-
+public_key, private_key = init_keys(address[1], password)
+client.send(f"{PUBLIC_KEY}{SEPARATOR}{public_key}".encode())
 threading.Thread(target=send, args=(client,)).start()
 threading.Thread(target=recv(client, address), args=(client,)).start()
