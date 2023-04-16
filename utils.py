d804073623cfe7fc65041af9316ec56a8b282b0b
BaranskiKacper <kbaranski20@gmail.com>
2023-04-16 15:55:05 +0200

    Initialization of RSA keys, password added, encrypt all

--- utils.py
+++ utils.py
@@ -8,26 +8,33 @@ import os
 # local key is used just for the PRIVATE KEY
 # use it during creation of the client
 def init_keys(client_id, local_key=None):
+    # hash the password (local key)
+    chosen_hash = hashes.SHA256()
+    hasher = hashes.Hash(chosen_hash)
+    hasher.update(local_key.encode())
+    hash_local_key = hasher.finalize()
+
     # check if are the keys exist if not create them
     try:
-        with open('keys/private_key/private_key_' + client_id + '.pem', 'rb') as private_key, \
-                open('keys/public_key/public_key_' + client_id + '.pem', 'rb') as public_key:
+        with open('keys/private_key/private_key_' + client_id + '.pem', 'rb') as _private_key, \
+                open('keys/public_key/public_key_' + client_id + '.pem', 'rb') as _public_key:
 
-            private_key = serialization.load_pem_private_key(
+            _private_key = serialization.load_pem_private_key(
                 private_key.read(),
-                password=local_key.encode()
+                password=hash_local_key.encode()
             )
-            public_key = serialization.load_pem_public_key(
+            _public_key = serialization.load_pem_public_key(
                 public_key.read(),
+                password=hash_local_key.encode()
             )
 
     except FileNotFoundError:
         # creation of the RSA keys to store on the hard disk
-        private_key = rsa.generate_private_key(
+        _private_key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048,
         )
-        public_key = private_key.public_key()
+        _public_key = private_key.public_key()
 
         # Serialize (changing the format) to store RSA keys
 
@@ -35,27 +42,26 @@ def init_keys(client_id, local_key=None):
         pem_private_key = private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
-            encryption_algorithm=serialization.BestAvailableEncryption(local_key.encode())
+            encryption_algorithm=serialization.BestAvailableEncryption(hash_local_key.encode())
         )
         with open('private_key_' + client_id + '.pem', 'wb') as f:
             f.write(pem_private_key)
 
-        # public key -> available for everyone (no password)
+        # public key -> password (local_key) not needed, but it is implemented
         pem_public_key = public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption(hash_local_key.encode())
         )
         with open('public_key_' + client_id + '.pem', 'wb') as f:
             f.write(pem_public_key)
 
-    return public_key, private_key
-
-#def read_public_key(client_id):
+    return _public_key, _private_key
 
 
 # sended_data -> file/text/png
 # mode -> CBC/ECB
-def encrypt(data, _mode):
+def encrypt(_public_key, data, _mode):
     # appends the bytes to make the sended_data the same size as block size (always 128 bytes for AES)
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(data) + padder.finalize()
@@ -77,11 +83,60 @@ def encrypt(data, _mode):
         file.write(_ciphertext)
 
     # place for encryption a session_key by rsa public_key
+    _session_key = _public_key.encrypt(
+        _session_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    _mode = _public_key.encrypt(
+        _mode.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    _iv = _public_key.encrypt(
+        _iv.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
 
     return _iv, _ciphertext, _session_key, _mode
 
 
-def decrypt(_ciphertext, _session_key, _mode, _iv=None):
+def decrypt(_private_key, _ciphertext, _session_key, _mode, _iv=None):
+    # decryption of the session key using private key
+    _session_key = _private_key.decrypt(
+        _session_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    _mode = _private_key.decrypt(
+        _mode.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    _iv = _private_key.decrypt(
+        _iv.encode(),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
     if _mode == 'CBC':
         cipher = Cipher(algorithms.AES256(session_key), modes.CBC(_iv))
     else:
@@ -107,5 +162,7 @@ if __name__ == '__main__':
             with open('sended_data/' + file_name, 'rb') as f:
                 byte_file = f.read()
                 # second arg pass by user
-                iv, ciphertext, session_key, mode = encrypt(byte_file, "ECB")
-                decrypt(ciphertext, session_key, mode, iv)
+                public_key = "kutas"
+                private_key = "sekretny kutas"
+                iv, ciphertext, session_key, mode = encrypt(public_key, byte_file, "ECB")
+                decrypt(private_key, ciphertext, session_key, mode, iv)
