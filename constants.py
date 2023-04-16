c95b58f3db7a178ea84eaf47b9d4466a111ac63c
michalbaldyga <michal.baldyga01@gmail.com>
2023-04-16 11:53:09 +0200

    Add constants

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on
+HOST = "127.0.0.1"          # Standard loopback interface address (localhost)
+PORT = 65432                # Port to listen on
+BUFFER_SIZE = 4096          # Send 4096 bytes each time step
+SEPARATOR = "<SEPARATOR>"   # SEPARATOR to separate the data fields
+MESSAGE_TAG = "<MSG>"
+FILE_TAG = "<FILE>"
+END_TAG = "<END>"
