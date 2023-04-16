44091236b01ed7356cbc4fa16ff613f00654fd8d
michalbaldyga <michal.baldyga01@gmail.com>
2023-04-16 19:11:28 +0200

    add login procedure and send public key

--- client.py
+++ client.py
@@ -1,20 +1,33 @@
 import socket
 import threading
-from constants import HOST, PORT, SEPARATOR, PUBLIC_KEY
+import os
+from cryptography.hazmat.primitives import hashes
+from constants import HOST, PORT, SEPARATOR, PUBLIC_KEY, PORT2, BUFFER_SIZE
 from message import send, recv
-from utils import init_keys
+from utils import init_keys, login
 
 # create the client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to our local address
+client.bind((HOST, PORT2))
 
 # connect to the server:
 print(f"[+] Connecting to {HOST}:{PORT}")
 client.connect((HOST, PORT))
 print("[+] Connected.")
-# type password
-password = input("Password: ")
-public_key, private_key = init_keys(PORT, password)
-client.send(f"{PUBLIC_KEY}{SEPARATOR}{public_key}".encode())
+
+# login/registration
+hash_password = login(PORT2)
+
+# generate public and private key
+public_key, private_key = init_keys(str(PORT2), hash_password)
+
+# send public key to other user
+client.send(public_key.encode())
+
+# recv other user public key
+received_key = client.recv(BUFFER_SIZE).decode()
 
 threading.Thread(target=send, args=(client,)).start()
 threading.Thread(target=recv(client, (HOST, PORT)), args=(client,)).start()
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 from constants import HOST, PORT, BUFFER_SIZE, MESSAGE_TAG, SEPARATOR, FILE_TAG, PUBLIC_KEY
 from message import send, recv
-from utils import init_keys
+from utils import init_keys, login
 
 # create the server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,12 +13,22 @@ server.bind((HOST, PORT))
 # enabling our server to accept connections
 server.listen()
 print(f"[*] Listening as {HOST}:{PORT}")
-# type password
-password = input("Password: ")
+
 # accept connection if there is any
 client, address = server.accept()
 print(f"[+] {address} is connected.")
-public_key, private_key = init_keys(address[1], password)
-client.send(f"{PUBLIC_KEY}{SEPARATOR}{public_key}".encode())
+
+# login/registration
+hash_password = login(PORT)
+
+# generate public and private key
+public_key, private_key = init_keys(str(PORT), hash_password)
+
+# send public key to other user
+client.send(public_key.encode())
+
+# recv other user public key
+received_key = client.recv(BUFFER_SIZE).decode()
+
 threading.Thread(target=send, args=(client,)).start()
 threading.Thread(target=recv(client, address), args=(client,)).start()
